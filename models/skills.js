f7f6da1d1c5457d140ed1f0225bb99dacea2591b
anami823 <anami823@gmail.com>
2019-08-13 19:37:45 -0700

    Create model of skills

---
+++ models/skills.js
@@ -0,0 +1,17 @@
+//create an array of "fake" data representing some of your awesome developer skills
+
+let skills = [
+    {
+    language: 'JS',
+    description: 'Object oriented language for both front and back-end developement',
+    level: 5
+    }
+]
+
+const getAll = () => {
+    return skills;
+}
+
+module.exports = {
+    getAll
+}
